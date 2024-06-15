3e031585cf2e75ec4721fad2fecc37c8826ac1f7
FrPawlik <Franek_Pawlik@wp.pl>
2024-06-15 15:33:06 +0200

    Changes to characterCreator class

--- Fantasy_battle_simulation/src/main/java/simulationsetup/CharacterCreator.java
+++ Fantasy_battle_simulation/src/main/java/simulationsetup/CharacterCreator.java
@@ -23,3 +23,87 @@ public class CharacterCreator {
     }
 
 }*/
+package simulationsetup;
+
+import characters.Character;
+import characters.warriors.Knight;
+import characters.warriors.Paladin;
+import characters.shooter.Ranger;
+import characters.shooter.Marksman;
+import characters.mage.Wizard;
+import characters.mage.Druid;
+import characters.rogue.Thief;
+import characters.rogue.Assassin;
+import inteligence.Agressive;
+import inteligence.InteligenceType;
+import inventory.Inventory;
+import map.Tile;
+import weapons.Weapon;
+
+import java.util.ArrayList;
+
+public class CharacterCreator {
+
+
+    public void CharacterCreation() {
+        int CounterId = 99;
+
+    }
+
+
+    public void createKnight(int id, Tile position, String Name) {
+        ArrayList<Weapon> Weapons = new ArrayList<>();
+        Inventory inventory = new Inventory();
+        InteligenceType intelligence = new Agressive(true);
+        characters.Character newKnight = new Knight(id, Name, intelligence, position, inventory);
+    }
+
+    public void createPaladin(int id, Tile position, String Name) {
+        ArrayList<Weapon> Weapons = new ArrayList<>();
+        Inventory inventory = new Inventory();
+        InteligenceType intelligence = new Agressive(true);
+        characters.Character newPaladin = new Paladin(id, Name, intelligence, position, inventory);
+    }
+
+    public void createRanger(int id, Tile position, String Name) {
+        ArrayList<Weapon> Weapons = new ArrayList<>();
+        Inventory inventory = new Inventory();
+        InteligenceType intelligence = new Agressive(true);
+        characters.Character newRanger = new Ranger(id, Name, intelligence, position, inventory);
+    }
+
+    public void createMarksman(int id, Tile position, String Name) {
+        ArrayList<Weapon> Weapons = new ArrayList<>();
+        Inventory inventory = new Inventory();
+        InteligenceType intelligence = new Agressive(true);
+        characters.Character newMarksman = new Marksman(id, Name, intelligence, position, inventory);
+    }
+
+    public void createDruid(int id, Tile position, String Name) {
+        ArrayList<Weapon> Weapons = new ArrayList<>();
+        Inventory inventory = new Inventory();
+        InteligenceType intelligence = new Agressive(true);
+        characters.Character newDruid = new Druid(id, Name, intelligence, position, inventory);
+    }
+
+    public void createWizard(int id, Tile position, String Name) {
+        ArrayList<Weapon> Weapons = new ArrayList<>();
+        Inventory inventory = new Inventory();
+        InteligenceType intelligence = new Agressive(true);
+        characters.Character newWizard = new Wizard(id, Name, intelligence, position, inventory);
+    }
+
+    public void createAssasin(int id, Tile position, String Name) {
+        ArrayList<Weapon> Weapons = new ArrayList<>();
+        Inventory inventory = new Inventory();
+        InteligenceType intelligence = new Agressive(true);
+        characters.Character newAssasin = new Assassin(id, Name, intelligence, position, inventory);
+    }
+
+    public void createThief(int id, Tile position, String Name) {
+        ArrayList<Weapon> Weapons = new ArrayList<>();
+        Inventory inventory = new Inventory();
+        InteligenceType intelligence = new Agressive(true);
+        characters.Character newThief = new Thief(id, Name, intelligence, position, inventory);
+    }
+}
