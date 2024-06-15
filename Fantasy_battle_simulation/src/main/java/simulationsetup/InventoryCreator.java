f63b20acf8af5e722cba54200259fed93b2a1a2b
FrPawlik <Franek_Pawlik@wp.pl>
2024-06-15 19:25:30 +0200

    further changes to InventoryCreator

--- Fantasy_battle_simulation/src/main/java/simulationsetup/InventoryCreator.java
+++ Fantasy_battle_simulation/src/main/java/simulationsetup/InventoryCreator.java
@@ -1,5 +1,16 @@
 package simulationsetup;
 
+import armours.Armour;
+import armours.HeavyArmour;
+import armours.LightArmour;
+import armours.MediumArmour;
+import inventory.items.HealPotion;
+import inventory.items.Item;
+import inventory.items.Shield;
+import weapons.*;
+
+import java.util.ArrayList;
+
 public class InventoryCreator {
     //Weapons
    private boolean fists = false;
@@ -54,10 +65,92 @@ public class InventoryCreator {
     public void setmedium(boolean bool){medium = bool;}
     public void setheavy(boolean bool){heavy = bool;}
 
+    public void setArmorToFalse()
+    {
+        setmedium(false);
+        setlight(false);
+        setheavy(false);
+    }
+
+    public void setWeaponsToFalse()
+    {
+        setfists(false);
+        setsword(false);
+        setbow(false);
+        setswordshield(false);
+        setstaff(false);
+        setdaggers(false);
+    }
+
+
+
+
     public void CreateInventory()
     {
 
     }
 
 
+    public ArrayList<Armour> AddArmorToInvenotry()
+    {
+        ArrayList<Armour> armours = new ArrayList<>();
+        if(light == true)
+        {
+            Armour lightArmour = new LightArmour();
+            armours.add(lightArmour);
+        }
+        if(medium == true)
+        {
+            Armour mediumArmour = new MediumArmour();
+            armours.add(mediumArmour);
+        }
+        if(heavy == true)
+        {
+            Armour heavyArmour = new HeavyArmour();
+            armours.add(heavyArmour);
+        }
+        return armours;
+    }
+
+
+    public ArrayList<Weapon> AddWeaponsToInventory() {
+        ArrayList<Weapon> weapons = new ArrayList<>();
+        if (fists == true) {
+            Weapon fists = new Fists();
+            weapons.add(fists);
+        }
+        if (sword == true) {
+            Weapon ssword = new Sword();
+            weapons.add(ssword);
+        }
+        if (bow == true) {
+            Weapon bow = new Bow();
+            weapons.add(bow);
+        }
+        if (swordshield == true) {
+            Weapon ssword = new Sword();
+            weapons.add(ssword);
+        }
+        if (staff == true) {
+            Weapon staff = new Staff();
+            weapons.add(staff);
+        }
+        if (daggers == true) {
+            Weapon daggers = new Dagger();
+            weapons.add(daggers);
+        }
+        return weapons;
+    }
+
+
+    public ArrayList<Item> AddItems()
+    {
+        ArrayList<Item> items = new ArrayList<>();
+        if(swordshield==true)
+        {
+            Shield shield = new Shield();
+            items.add(shield);
+        }
+        return items;
+    }
 }
