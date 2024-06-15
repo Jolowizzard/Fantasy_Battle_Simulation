e45dcd9cecaea01fe550a4702c84509186316173
FrPawlik <Franek_Pawlik@wp.pl>
2024-06-15 18:36:42 +0200

    addition of InventoryCreator class

---
+++ Fantasy_battle_simulation/src/main/java/simulationsetup/InventoryCreator.java
@@ -0,0 +1,63 @@
+package simulationsetup;
+
+public class InventoryCreator {
+    //Weapons
+   private boolean fists = false;
+   private boolean sword = false;
+    private boolean swordshield = false;
+    private boolean bow = false;
+    private boolean staff = false;
+    private boolean daggers = false;
+
+    public boolean getfists(){return fists;}
+    public boolean getsword(){return sword;}
+    public boolean getbow(){return bow;}
+    public boolean getswordshield(){return swordshield;}
+    public boolean getstaff(){return staff;}
+    public boolean getdaggers(){return daggers;}
+
+    public void setfists(boolean bool)
+    {
+        fists = bool;
+    }
+
+    public void setsword(boolean bool)
+    {
+        sword = bool;
+    }
+    public void setbow(boolean bool){bow = bool;}
+
+    public void setswordshield(boolean bool)
+    {
+        swordshield = bool;
+    }
+
+    public void setstaff(boolean bool)
+    {
+        staff = bool;
+    }
+
+    public void setdaggers(boolean bool)
+    {
+        daggers = bool;
+    }
+    //Armor
+    private boolean light =false;
+    private boolean medium =false;
+    private boolean heavy =false;
+
+    public boolean getlight(){return light;}
+    public boolean getmedium(){return medium;}
+    public boolean getheavy(){return heavy;}
+
+    public void setlight(boolean bool){light = bool;}
+    public void setmedium(boolean bool){medium = bool;}
+    public void setheavy(boolean bool){heavy = bool;}
+
+    public void CreateInventory()
+    {
+
+    }
+
+
+}
